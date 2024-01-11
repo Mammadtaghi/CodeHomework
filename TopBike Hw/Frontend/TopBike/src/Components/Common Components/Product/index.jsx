6e051a61e02ef115cf7f5224748f9c1cb2b71ddc
Mammadtaghi <tu7k3zlya@code.edu.az>
2024-01-11 08:36:53 +0400

    Some changes added

--- TopBike Hw/Frontend/TopBike/src/Components/Common Components/Product/index.jsx
+++ TopBike Hw/Frontend/TopBike/src/Components/Common Components/Product/index.jsx
@@ -7,16 +7,18 @@ function Product({ id, image, title, price, discount }) {
     <div id={style.Product}>
       <div className={style.imgBox}>
         <img src={image} alt={title} />
-        <div className={style.iconsContainer}>
-          <div className={style.iconBox}><i className='fa-regular fa-heart'></i></div>
-          <div className={style.iconBox}><i className='fa-solid fa-shopping-bag'></i></div>
-          <div className={style.iconBox}><i className='fa-solid fa-magnifying-glass'></i></div>
+        <div className={style.iconModal}>
+          <div className={style.iconsContainer}>
+            <div className={style.iconBox}><i className='fa-regular fa-heart'></i></div>
+            <div className={style.iconBox}><i className='fa-solid fa-shopping-bag'></i></div>
+            <div className={style.iconBox}><i className='fa-solid fa-magnifying-glass'></i></div>
+          </div>
         </div>
-        { discount ? <div className={style.discountBox}>-{discount}%</div> : null}
+        {discount ? <div className={style.discountBox}>-{discount}%</div> : null}
       </div>
       <div className={style.textBox}>
         <h4 className={style.title}>{title}</h4>
-        <p className={style.priceBox}><span style={discount ? {color:"grey", textDecoration:"line-through"} : {color:'#ffaa00'}}>${price}.00</span>{ discount ? <span style={{color:'#ffaa00'}}>${(price*(100-discount)/100)}.00</span> : null}</p>
+        <p className={style.priceBox}><span style={discount ? { color: "grey", textDecoration: "line-through" } : { color: '#ffaa00' }}>${price}.00</span>{discount ? <span style={{ color: '#ffaa00' }}>${(price * (100 - discount) / 100)}.00</span> : null}</p>
       </div>
     </div>
   )
